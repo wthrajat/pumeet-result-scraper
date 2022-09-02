f5504a141d68687674a6d113c2c44174ab53d38d
Rajat Khanduri <38693805+wthrajat@users.noreply.github.com>
2022-09-02 16:38:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+#Importing Shimporting
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import time
+import os
+    #Path = your path of chrome webdriver here
+#driver = webdriver.Chrome(Path)
+driver.get("https://results.puexam.in/HomePage.aspx")
+driver.find_element(By.ID, "ctl00_LinkButton_Entrance").click()
+driver.find_element(By.ID, "ctl00_cph1_lbtn_PUM").click()
+
+for roll in range(900001, 900394):
+    if roll == 900040 or roll == 900390 or roll == 900346 or roll == 900336 or roll == 900333 or roll == 900307 or roll == 900306 or roll == 900300 or roll == 900279 or roll == 900049 or roll == 900053 or roll == 900061 or roll == 900088 or roll == 900095 or roll == 900114 or roll == 900122 or roll == 900152 or roll == 900158 or roll == 900162 or roll == 900171 or roll == 900174 or roll == 900192 or roll == 900207 or roll == 900277 or roll == 900268:
+        continue
+    driver.find_element(By.ID, "ctl00_cph1_txtRollNo").send_keys(roll)
+    driver.find_element(By.ID, "ctl00_cph1_btnShowResult").click()
+    # isPresent = driver.find_element(By.ID, "ctl00_cph1_lblResult")
+    # print(isPresent.is_displayed())
+    # if driver.find_element(By.ID, "ctl00_cph1_lblResult").is_displayed() is False:
+    # ifGaandPhulanaIsAnArt.thenYouAreMasterOfIt(True)
+
+    #Attributes
+    name = driver.find_element(By.ID, "ctl00_cph1_lblCName").text
+
+    marks = driver.find_element(By.ID, "ctl00_cph1_lblMarks").text
+    rank = driver.find_element(By.ID, "ctl00_cph1_lblRank").text
+    rollNo = driver.find_element(By.ID, "ctl00_cph1_lblRollNo").text
+
+    #You may add sleep timer if u stalking by the "eyes"
+    # time.sleep(x) x==in seconds
+    # For console: line = rollNo + "," + name + "," + marks + "," + rank
+    driver.find_element(By.ID, "ctl00_cph1_btnBack").click()
+    roll += 1
+
+print("\nJai Shri Parshuraam!")
