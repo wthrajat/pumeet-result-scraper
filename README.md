a9c43cc581de36c4b7c64a624f0aee9bd2ab2e60
wthrajat <rajatkhanduri290102@gmail.com>
2023-08-13 22:30:38 +0530

    absentees logic to use try catch
    Signed-off-by: wthrajat <rajatkhanduri290102@gmail.com>

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
-# What is this?
+## What is this?
 See description
 
-# How to use this?
-1. Install Selenium ``pip install selenium`` and Browser Webdriver [here](https://www.selenium.dev/documentation/webdriver/getting_started/install_drivers/) 
+## How to use this?
 
-2. Put Browser Webdriver's executable path in the path variable
+Just run the script & see the output in the console/terminal
 
-    (Note that in Windows you need to include ``filename.exe`` in the path also)
+### Pre-requisites
+1. Selenium `pip install selenium`
+2. Webdriver for your browser (Using any `Chromium` based browser is recommended for this):
+    - For Chrome, install [chromedriver](https://chromedriver.chromium.org/downloads)
+    - For Firefox, install [geckowebdriver](https://github.com/mozilla/geckodriver/releases)
 
-3. Sit back and run the script
+3. Shortcut: if the webdriver isn't installed in the system path, you can just provide its location as:
+```python
+    driver = webdriver.Chrome("/path/to/webdriver")
+    # OR
+    driver = webdriver.Firefox("/path/to/webdriver")
+```
+
 4. (Optional) Print to the console or use Pandas library to import this to a python list or whatever to sort it according to the rank.
-(Sorted version in Assets)
+(Sorted version in `./assets`)
 
-# Why?
+## Why?
 Many people wanted to see the Marks vs Rank data for this year's PUMEET
 
--- script.py
+++ script.py
@@ -1,36 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 import time
-import os
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 driver.get("https://results.puexam.in/HomePage.aspx")
 driver.find_element(By.ID, "ctl00_LinkButton_Entrance").click()
+driver.find_element(By.ID, "ctl00_LinkButton7").click()
 driver.find_element(By.ID, "ctl00_cph1_lbtn_PUM").click()
 
-for roll in range(1000001, 1000394):
-    if roll == 900040 or roll == 900390 or roll == 900346 or roll == 900336 or roll == 900333 or roll == 900307 or roll == 900306 or roll == 900300 or roll == 900279 or roll == 900049 or roll == 900053 or roll == 900061 or roll == 900088 or roll == 900095 or roll == 900114 or roll == 900122 or roll == 900152 or roll == 900158 or roll == 900162 or roll == 900171 or roll == 900174 or roll == 900192 or roll == 900207 or roll == 900277 or roll == 900268:
-        continue
+for roll in range(900001, 900394):
+
+    driver.find_element(By.ID, "ctl00_cph1_txtRollNo").clear()
     driver.find_element(By.ID, "ctl00_cph1_txtRollNo").send_keys(roll)
     driver.find_element(By.ID, "ctl00_cph1_btnShowResult").click()
-    # isPresent = driver.find_element(By.ID, "ctl00_cph1_lblResult")
-    # print(isPresent.is_displayed())
-    # if driver.find_element(By.ID, "ctl00_cph1_lblResult").is_displayed() is False:
-    # ifGaandPhulanaIsAnArt.thenYouAreMasterOfIt(True)
 
-    #Attributes
-    name = driver.find_element(By.ID, "ctl00_cph1_lblCName").text
+    if "Absent" not in driver.page_source:
+        name = driver.find_element(By.ID, "ctl00_cph1_lblCName").text
+        marks = driver.find_element(By.ID, "ctl00_cph1_lblMarks").text
+        rank = driver.find_element(By.ID, "ctl00_cph1_lblRank").text
+        rollNo = driver.find_element(By.ID, "ctl00_cph1_lblRollNo").text
 
-    marks = driver.find_element(By.ID, "ctl00_cph1_lblMarks").text
-    rank = driver.find_element(By.ID, "ctl00_cph1_lblRank").text
-    rollNo = driver.find_element(By.ID, "ctl00_cph1_lblRollNo").text
+        print(rollNo + "," + name + "," + marks + "," + rank)
+    else:
+        print(f"{roll} was absent lmao 2200 Rs ka choona")
 
-    #You may add sleep timer if u stalking by the "eyes"
-    # time.sleep(x) x==in seconds
-    line = rollNo + "," + name + "," + marks + "," + rank
-    print(line)
     driver.find_element(By.ID, "ctl00_cph1_btnBack").click()
-    roll += 1
 
 print("\nJai Shri Parshuraam!")
+
+driver.quit()
