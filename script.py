5a8723ae1c24b51e56853ddefb946e2dc6f5595c
wthrajat <rajatkhanduri290102@gmail.com>
2023-08-13 20:36:01 +0530

    minor fix

--- script.py
+++ script.py
@@ -1,17 +1,15 @@
-#Importing Shimporting
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 import time
 import os
-    #Path = your path of chrome webdriver here
-#driver = webdriver.Chrome(Path)
+
+driver = webdriver.Firefox()
 driver.get("https://results.puexam.in/HomePage.aspx")
 driver.find_element(By.ID, "ctl00_LinkButton_Entrance").click()
 driver.find_element(By.ID, "ctl00_cph1_lbtn_PUM").click()
 
-for roll in range(900001, 900394):
+for roll in range(1000001, 1000394):
     if roll == 900040 or roll == 900390 or roll == 900346 or roll == 900336 or roll == 900333 or roll == 900307 or roll == 900306 or roll == 900300 or roll == 900279 or roll == 900049 or roll == 900053 or roll == 900061 or roll == 900088 or roll == 900095 or roll == 900114 or roll == 900122 or roll == 900152 or roll == 900158 or roll == 900162 or roll == 900171 or roll == 900174 or roll == 900192 or roll == 900207 or roll == 900277 or roll == 900268:
         continue
     driver.find_element(By.ID, "ctl00_cph1_txtRollNo").send_keys(roll)
@@ -30,7 +28,8 @@ for roll in range(900001, 900394):
 
     #You may add sleep timer if u stalking by the "eyes"
     # time.sleep(x) x==in seconds
-    # For console: line = rollNo + "," + name + "," + marks + "," + rank
+    line = rollNo + "," + name + "," + marks + "," + rank
+    print(line)
     driver.find_element(By.ID, "ctl00_cph1_btnBack").click()
     roll += 1
 
