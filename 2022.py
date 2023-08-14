7b68bc96e47e92c1215624a9d93698309269d8b4
wthrajat <rajatkhanduri290102@gmail.com>
2023-08-15 00:31:45 +0530

    disqualified logic improved

--- 2022.py
+++ 2022.py
@@ -11,13 +11,13 @@ driver.find_element(By.ID, "ctl00_cph1_lbtn_PUM").click()
 print("{:<10} {:<30} {:<10} {:<10}".format("RollNo", "Name", "Marks", "Rank"))
 print("="*70)
 
-for roll in range(900001, 900394):
+for roll in range(900094, 900394):
 
     driver.find_element(By.ID, "ctl00_cph1_txtRollNo").clear()
     driver.find_element(By.ID, "ctl00_cph1_txtRollNo").send_keys(roll)
     driver.find_element(By.ID, "ctl00_cph1_btnShowResult").click()
 
-    if "Absent" not in driver.page_source or "Not Qualified" not in driver.page_source:
+    if (("Not Qualified" not in driver.page_source) and ("Absent" not in driver.page_source)):
         name = driver.find_element(By.ID, "ctl00_cph1_lblCName").text
         marks = driver.find_element(By.ID, "ctl00_cph1_lblMarks").text
         rank = driver.find_element(By.ID, "ctl00_cph1_lblRank").text
@@ -26,7 +26,7 @@ for roll in range(900001, 900394):
         line = "{:<10} {:<30} {:<10} {:<10}".format(rollNo, name, marks, rank)
         print(line)
     else:
-        print(f"{roll} was absent, lmao 2200/- ka choona lag gaya!")
+        print(f"{roll} was absent/disqualified, lmao 2200/- ka choona lag gaya!")
     driver.find_element(By.ID, "ctl00_cph1_btnBack").click()
 
 print("="*70)
